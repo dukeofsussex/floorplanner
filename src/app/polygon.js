c5e1dbd22e62cf2817895fedc56fe11177e96939
Rob Storm <rstorm000@gmail.com>
2018-03-27 19:01:14 -0400

    Change hover description look so that it always appears just down and to the right a certain amount

--- src/app/polygon.js
+++ src/app/polygon.js
@@ -191,8 +191,11 @@ export default class Polygon {
 
                 const coords = d3.mouse(this);
 
-                self.tooltip.style('left', `${coords[0] + 15}px`) // 15 pixels for the row margin
-                    .style('top', `${coords[1] + (self.tooltip.node().getBoundingClientRect().height / 2)}px`);
+                // self.tooltip.style('left', `${coords[0] + 15}px`) // 15 pixels for the row margin
+                //     .style('top', `${coords[1] + (self.tooltip.node().getBoundingClientRect().height / 2)}px`);
+                
+                self.tooltip.style('left', `${coords[0] + 25}px`) // Rob version just sets it away from the corner
+                    .style('top', `${coords[1] + 100}px`);
             })
             .on('mouseout', (d) => {
                 if (this.editing) {
