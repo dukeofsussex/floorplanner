40a38603309707e84f410135c5f0ab6ab087d9d4
Christopher Satchell <satchell028@gmail.com>
2018-03-27 19:21:20 +0200

    Display HTML as HTML

--- style.js
+++ style.js
@@ -61,12 +61,12 @@ var FloorPlanner =
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 475);
+/******/ 	return __webpack_require__(__webpack_require__.s = 476);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 475:
+/***/ 476:
 /***/ (function(module, exports) {
 
 // removed by extract-text-webpack-plugin
