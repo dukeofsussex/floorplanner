8ae193ed4ea30e74e371c78b771638fce807b1a0
Christopher Satchell <satchell028@gmail.com>
2018-04-23 16:01:19 +0200

    Prepare storage for a proper floorplan model

--- src/storage.js
+++ src/storage.js
@@ -1,64 +1,68 @@
+const defaultTestFloorplan = [
+    {
+        image: 'https://i.imgur.com/Y9cDiqE.png',
+        name: 'Riverside Main Floor',
+        description: 'Just some random wacky description',
+        areas: [
+            {
+                uid: 'ajf9v0',
+                points: [{ x: 52, y: 92 }, { x: 50, y: 151 }, { x: 123, y: 149 }, { x: 125, y: 91 }],
+                name: 'Back stairwell',
+                description: 'Stairs',
+                hoverDescription: 'Hover \'n stuff',
+            }, {
+                uid: 'ufnf12',
+                points: [{ x: 450, y: 250 }, { x: 700, y: 250 }, { x: 700, y: 275 }, { x: 450, y: 275 }],
+                name: 'Some botched bridge stairwell',
+                description: 'Woopsee',
+                hoverDescription: 'Fucked this one right up',
+            },
+        ],
+    },
+];
+
 export default class Storage {
     constructor() {
-        this.localStorageKey = 'floorplanner_floors';
+        this.localStorageKey = 'floorplanner:floorplan';
 
         if (localStorage.getItem(this.localStorageKey) === null) {
-            this.floors = [
-                {
-                    image: 'https://i.imgur.com/Y9cDiqE.png',
-                    name: 'Riverside Main Floor',
-                    description: 'Just some random wacky description',
-                    areas: [
-                        {
-                            uid: 'ajf9v0',
-                            points: [{ x: 52, y: 92 }, { x: 50, y: 151 }, { x: 123, y: 149 }, { x: 125, y: 91 }],
-                            name: 'Back stairwell',
-                            description: 'Stairs',
-                            hoverDescription: 'Hover \'n stuff',
-                        }, {
-                            uid: 'ufnf12',
-                            points: [{ x: 450, y: 250 }, { x: 700, y: 250 }, { x: 700, y: 275 }, { x: 450, y: 275 }],
-                            name: 'Some botched bridge stairwell',
-                            description: 'Woopsee',
-                            hoverDescription: 'Fucked this one right up',
-                        },
-                    ],
-                },
-            ];
+            this.floorplan = defaultTestFloorplan;
         } else {
-            this.floors = JSON.parse(localStorage.getItem(this.localStorageKey));
+            this.floorplan = JSON.parse(localStorage.getItem(this.localStorageKey));
         }
     }
 
-    import(floors) {
-        this.floors = JSON.parse(floors);
+    import(floorplan) {
+        this.floorplan = JSON.parse(floorplan);
         this.saveToLocalStorage();
     }
 
     export() {
-        return JSON.stringify(this.floors);
+        return JSON.stringify(this.floorplan);
     }
 
     addFloor(floor) {
-        this.floors.push(floor);
-        return this.floors.length - 1;
+        this.floorplan.push(floor);
+        this.saveToLocalStorage();
+        return this.floorplan.length - 1;
     }
 
     getFloor(index) {
-        return this.floors[index] || null;
+        return this.floorplan[index] || null;
     }
 
     updateFloor(floor, index) {
-        this.floors[index] = floor;
+        this.floorplan[index] = floor;
+        this.saveToLocalStorage();
+    }
+
+    deleteFloor(index) {
+        this.floorplan.splice(index, 1);
         this.saveToLocalStorage();
     }
 
     saveToLocalStorage() {
-        localStorage.setItem(this.localStorageKey, JSON.stringify(this.floors));
-    }
-
-    deleteFloor(index) {
-        this.floors.splice(index, 1);
+        localStorage.setItem(this.localStorageKey, JSON.stringify(this.floorplan));
     }
 
     deleteLocalStorage() {
